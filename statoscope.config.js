d75af108076bfffd07b68d2e6def73ac10acb1a7
nickshevr <nickshevr@gmail.com>
2022-11-07 18:34:43 +0300

    Update statoscope.config.js

--- statoscope.config.js
+++ statoscope.config.js
@@ -3,7 +3,7 @@ module.exports = {
         plugins: ['@statoscope/webpack'],
         rules: {
             '@statoscope/webpack/build-time-limits': ['error', 10000],
-            '@statoscope/webpack/restricted-packages': ['error', ['bn.js', 'browserify-crypto']],
+            '@statoscope/webpack/restricted-packages': ['error', ['bn.js', 'crypto-browserify']],
             '@statoscope/webpack/entry-download-size-limits': ['error', { global: { maxSize: 3 * 1024 * 100 } }],
             '@statoscope/webpack/no-packages-dups': ['error'],
         }
